8be3961e3f45ee3150861b301d78e73649e03882
Edson Martins <edsonmartins2005@gmail.com>
2020-07-19 18:10:12 -0300

    ok

--- anteros-react-datasource/build_local.sh
+++ anteros-react-datasource/build_local.sh
@@ -1,6 +1,6 @@
 npm run dev
 npm run build
 npm pack
-npm unpublish --registry http://vps4657.publiccloud.com.br:4873 --force
-npm publish --registry http://vps4657.publiccloud.com.br:4873
+npm unpublish --registry http://ec2-54-232-145-45.sa-east-1.compute.amazonaws.com:4873 --force
+npm publish --registry http://ec2-54-232-145-45.sa-east-1.compute.amazonaws.com:4873
 
