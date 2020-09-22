ac56059289eaf01c4715969f2cd00c62a41f1bff
Danilo Sambugaro <dan.sambugaro@gmail.com>
2020-09-22 09:08:47 -0300

    Ajustando versões

--- compilar_publicar_projeto.sh
+++ compilar_publicar_projeto.sh
@@ -1,60 +1,46 @@
-cd anteros-react-admin
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-audiovideo
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-buttons
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-calendar
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
+# cd anteros-react-core
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
+# cd anteros-react-collections
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
+#cd anteros-react-dropzone
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
+# cd anteros-react-oauth2
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
+# cd anteros-react-theme
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
+# cd anteros-react-designeditor
+# npm install
+# npm run dev
+# npm run build
+# npm pack
+# npm publish --access public
+# cd ..
 cd anteros-react-charts
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-collections
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-containers
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-core
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-dashboard
 npm install
 npm run dev
 npm run build
@@ -68,56 +54,7 @@ npm run build
 npm pack
 npm publish --access public
 cd ..
-cd anteros-react-designeditor
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-documentation
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-dropzone
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-editors
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
 cd anteros-react-gridgallery
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-image
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-kanban
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-label
 npm install
 npm run dev
 npm run build
@@ -131,35 +68,7 @@ npm run build
 npm pack
 npm publish --access public
 cd ..
-cd anteros-react-list
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-loaders
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
 cd anteros-react-maps
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-masonry
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-menu
 npm install
 npm run dev
 npm run build
@@ -173,7 +82,7 @@ npm run build
 npm pack
 npm publish --access public
 cd ..
-cd anteros-react-navigation
+cd anteros-react-masonry
 npm install
 npm run dev
 npm run build
@@ -194,7 +103,112 @@ npm run build
 npm pack
 npm publish --access public
 cd ..
-cd anteros-react-oauth2
+cd anteros-react-security
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-treeview
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-table
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-list
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-kanban
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-label
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-loaders
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-buttons
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-image*
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-menu*
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-containers
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-calendar
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-editors
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-navigation
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-dashboard
+npm install
+npm run dev
+npm run build
+npm pack
+npm publish --access public
+cd ..
+cd anteros-react-audiovideo
 npm install
 npm run dev
 npm run build
@@ -215,35 +229,7 @@ npm run build
 npm pack
 npm publish --access public
 cd ..
-cd anteros-react-security
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-table
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-template
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-theme
-npm install
-npm run dev
-npm run build
-npm pack
-npm publish --access public
-cd ..
-cd anteros-react-treeview
+cd anteros-react-admin
 npm install
 npm run dev
 npm run build
